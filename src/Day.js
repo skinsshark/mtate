300594194189a3756c9dcc943905f790c8a3068d
SHARON ZHENG <sharzheng@gmail.com>
2020-04-12 22:25:18 -0400

    use prop as filename (setup)

--- src/Day.js
+++ src/Day.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Day.css';
 
 import lottie from 'lottie-web';
-import animationData from './lottie/0412.json';
 
 class Day extends React.Component {
   componentDidMount() {
+    const { stamp } = this.props;
+    
     //call the loadAnimation to start the animation
     lottie.loadAnimation({
       container: this.animBox,
       renderer: 'svg',
       loop: true,
       autoplay: true,
-      animationData
+      animationData: require(`./lottie/${stamp}.json`)
     });
   }
 
