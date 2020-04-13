300af4dc8f51bd8b0ea5460f1a23689f3182d6ce
SHARON ZHENG <sharzheng@gmail.com>
2020-04-12 20:22:57 -0400

    Updates

---
+++ precache-manifest.52b84c23d51cd33c56b103d8001ab6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b22fc621432b1503419796e5e9f391",
+    "url": "/mtate/index.html"
+  },
+  {
+    "revision": "680bb2ba84cf19838b5b",
+    "url": "/mtate/static/css/main.a5c28fff.chunk.css"
+  },
+  {
+    "revision": "6f4fb0417da34e617593",
+    "url": "/mtate/static/js/2.e404e590.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/mtate/static/js/2.e404e590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680bb2ba84cf19838b5b",
+    "url": "/mtate/static/js/main.0ea573bb.chunk.js"
+  },
+  {
+    "revision": "58305c45e6bd025ccc68",
+    "url": "/mtate/static/js/runtime-main.ca754b51.js"
+  }
+]);
